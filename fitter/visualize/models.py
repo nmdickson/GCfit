2ab3663a66a971ee70aacf0660b513d3dac7afbf
Nolan Dickson <ndickson@protonmail.com>
2021-06-10 12:16:24 -0400

    change LOS plot to mas/yr

--- fitter/visualize/models.py
+++ fitter/visualize/models.py
@@ -374,7 +374,7 @@ class ModelVisualizer(_Visualizer):
         ax.set_xscale("log")
 
         model_r = self.model.r.to(u.arcsec)
-        model_LOS = np.sqrt(self.model.v2pj[mass_bin])
+        model_LOS = np.sqrt(self.model.v2pj[mass_bin]).to('mas/yr')
 
         if show_obs:
             try:
@@ -383,7 +383,7 @@ class ModelVisualizer(_Visualizer):
                 xerr = self.get_err(LOS, 'r')
                 yerr = self.get_err(LOS, 'σ')
 
-                ax.errorbar(LOS['r'], LOS['σ'], fmt='k.',
+                ax.errorbar(LOS['r'], LOS['σ'].to('mas/yr'), fmt='k.',
                             xerr=xerr, yerr=yerr)
 
                 if hyperparam:
