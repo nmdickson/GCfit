0345a772cb62b067d669daa0e1689cd2c00c02b5
Nolan Dickson <ndickson@protonmail.com>
2022-01-27 14:07:13 -0400

    remove backend "initial batch" logic
    The whole idea behind using the generator results was to avoid
    messing up the results, until they were combined for each batch.
    But in the case of the initial batch, this doesn't make sense,
    as there is no pre-existing results to mess up.
    So we can simply store the entire on-the-fly results during that
    sampling stage.

--- fitter/core/main.py
+++ fitter/core/main.py
@@ -226,66 +226,6 @@ class NestedSamplingOutput(Output):
             for key, val in results.items():
                 grp[key][-n_grow:] = val
 
-    # ----------------------------------------------------------------------
-    # Tracking of initial batch sampling
-    # ----------------------------------------------------------------------
-
-    _initial_batch_keys = ('worst', 'ustar', 'vstar', 'loglstar', 'logvol',
-                           'logwt', 'logz', 'logzvar', 'h', 'ncall',
-                           'worst_orig', 'bound_orig', 'bound_iter', 'eff',
-                           'delta_logz')
-
-    def create_initial_batch(self, *, strict=False):
-        '''initialize the initial batch group'''
-
-        with self.open('a') as hdf:
-
-            base_grp = hdf.require_group(name=self.group)
-
-            if 'initial_batch' not in base_grp:
-                grp = base_grp.create_group(name='initial_batch')
-
-                # Two-dimensional datasets
-                for k in {'vstar', 'ustar'}:
-                    grp.create_dataset(k, shape=(0, self.ndim),
-                                       maxshape=(None, self.ndim))
-
-                # One-dimensional datasets
-                for k in set(self._initial_batch_keys) - {'ustar', 'vstar'}:
-                    grp.create_dataset(k, shape=(0,), maxshape=(None,))
-
-            elif strict:
-                mssg = 'initial_batch already exists'
-                raise ValueError(mssg)
-
-    def _grow_initial_batch(self, n_grow=1):
-        '''called in the background to dynamically resize the relevant datasets
-        '''
-
-        with self.open('r+') as hdf:
-            base_grp = hdf.require_group(name=self.group)
-
-            grp = base_grp['initial_batch']
-
-            for k in self._initial_batch_keys:
-                grp[k].resize(grp['vstar'].shape[0] + n_grow, axis=0)
-
-    def update_intial_batch(self, results):
-        '''Append to the "initial batch" the results of each sampling iteration
-        '''
-
-        results = dict(zip(self._initial_batch_keys, results))
-
-        n_grow = results['vstar'].shape[0]
-        self._grow_initial_batch(n_grow)
-
-        with self.open('a') as hdf:
-            base_grp = hdf.require_group(name=self.group)
-            grp = base_grp['initial_batch']
-
-            for key, val in results.items():
-                grp[key][-n_grow:] = val
-
 
 def MCMC_fit(cluster, Niters, Nwalkers, Ncpu=2, *,
              mpi=False, initials=None, param_priors=None, moves=None,
@@ -893,12 +833,9 @@ def nested_fit(cluster, *, bound_type='multi', sample_type='auto',
         stop_kw = {'pfrac': pfrac, 'n_mc': 0, 'target_neff': eff_samples}
         weight_kw = {'pfrac': pfrac, 'maxfrac': maxfrac}
 
-        # runs an initial set of set samples, as if using `NestedSampler`
-        # TODO I'm not sure how to handle "initial_batch" on restarted runs
-        backend.create_initial_batch()
-
-        for results in sampler.sample_initial(**initial_kwargs):
-            backend.update_intial_batch(results)
+        # TODO have to atleast supply nlive here? so maybe add to defaults
+        for _ in sampler.sample_initial(**initial_kwargs):
+            backend.store_results(sampler.results)
 
         backend.store_results(sampler.results)
 
@@ -917,6 +854,7 @@ def nested_fit(cluster, *, bound_type='multi', sample_type='auto',
             logging.info(f"Sampling new batch bebtween logl bounds {wt} ({tn})")
 
             for results in sampler.sample_batch(logl_bounds=wt, **batch_kwargs):
+                # Shouldn't store entire results until they're combined below
                 backend.update_current_batch(results)
 
             logging.info("Combining batch with existing results")
