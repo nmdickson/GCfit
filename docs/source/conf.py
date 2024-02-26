f97e92df6c67ac7bcfed742f5f7a3809f169d15b
Nolan Dickson <ndickson@protonmail.com>
2024-02-26 13:41:26 -0400

    update paper and year references

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,11 +18,11 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 
 project = 'GCfit'
-copyright = '2023, Nolan Dickson'
+copyright = '2024, Nolan Dickson'
 author = 'Nolan Dickson'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '1.1'
 
 
 # -- General configuration ---------------------------------------------------
