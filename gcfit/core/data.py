e0210fec79460d9c77bdeb04e59d5af3443662aa
Nolan Dickson <nolan.dickson@smu.ca>
2026-01-08 14:25:06 -0400

    add more args surrounding equipartition in limepy
    This includes the new zeta parameter for massive objects, and eta
    for mass-dependent anisotropy.
    
    Also bumps the default upper IMF limit from 100 to 150, but that has
    been used in most places for a while now.

--- gcfit/core/data.py
+++ gcfit/core/data.py
@@ -850,7 +850,7 @@ class Model(lp.limepy):
 
     m_breaks : (4,) numpy.ndarray or astropy.Quantity, optional
         The IMF break-masses (including outer bounds) in Msun, defining the
-        mass ranges of each IMF exponent. Defaults to [0.1, 0.5, 1.0, 100].
+        mass ranges of each IMF exponent. Defaults to [0.1, 0.5, 1.0, 150].
 
     nbins : (3,) numpy.ndarray of int, optional
         Number of mass bins in each regime of the IMF, as defined by `m_breaks`.
@@ -1092,13 +1092,14 @@ class Model(lp.limepy):
     def __init__(self, W0, M, rh, g=1.5, delta=0.45, ra=1e8,
                  a1=1.3, a2=2.3, a3=2.3, BHret=5.0, d=5,
                  s2=0., F=1., *, observations=None, age=None, FeH=None,
-                 m_breaks=[0.1, 0.5, 1.0, 100], nbins=[5, 5, 20], meq=0.0,
+                 m_breaks=[0.1, 0.5, 1.0, 150], nbins=[5, 5, 20],
                  tracer_masses=None, tcc=0.0, NS_ret=0.1, BH_ret_int=1.0,
+                 meq=0.0, eta=0.0, zeta=1.0,
                  esc_rate=0.0, natal_kicks=True, kick_method='maxwellian',
                  f_kick=None, SNe_method='rapid', vesc=90, kick_vdisp=265.,
                  kick_slope=1, kick_scale=20, MF_kwargs=None,
                  meanmassdef='global', ode_maxstep=1e10, ode_rtol=1e-7,
-                 diffcrit=1e-8, max_mf_iter=100):
+                 diffcrit=1e-8, max_mf_iter=100, mf_iter_index=0.5):
 
         # ------------------------------------------------------------------
         # Add/convert units of some quantities. Supports quantities as inputs
@@ -1228,6 +1229,8 @@ class Model(lp.limepy):
             rh=rh.value,
             ra=ra.value,
             delta=delta,
+            eta=eta,
+            zeta=zeta,
             mj=mj,
             Mj=Mj,
             meq=meq,
@@ -1237,7 +1240,8 @@ class Model(lp.limepy):
             max_step=ode_maxstep,
             ode_rtol=ode_rtol,
             diffcrit=diffcrit,
-            max_mf_iter=max_mf_iter
+            max_mf_iter=max_mf_iter,
+            mf_iter_index=mf_iter_index
         )
 
         try:
@@ -1738,9 +1742,10 @@ class EvolvedModel(Model):
     def __init__(self, W0, M0, rh0, g=1.5, delta=0.45, ra=1e8,
                  a1=1.3, a2=2.3, a3=2.3, d=5,
                  s2=0., F=1., *, observations=None, age=None, FeH=None,
-                 Zsun=0.02, m_breaks=[0.1, 0.5, 1.0, 100], nbins=[5, 5, 20],
+                 Zsun=0.02, m_breaks=[0.1, 0.5, 1.0, 150], nbins=[5, 5, 20],
                  tracer_masses=None, tcc=0.0, NS_ret=0.1, BH_ret_int=1.0,
-                 meq=0.0, md=1.2, natal_kicks=True, kick_method='maxwellian',
+                 meq=0.0, eta=0.0, zeta=1.0, md=1.2,
+                 natal_kicks=True, kick_method='maxwellian',
                  f_kick=None, SNe_method='rapid', kick_vdisp=265.,
                  kick_slope=1, kick_scale=20,
                  cbh_kwargs=None, MF_kwargs=None, meanmassdef='global',
@@ -1912,7 +1917,8 @@ class EvolvedModel(Model):
 
         # Explicitly specify everything so we can get the correct Signature
         super().__init__(W0, M, rh, g=g, delta=delta, ra=ra,
-                         a1=a1, a2=a2, a3=a3, BHret=BHret, d=d, meq=meq,
+                         a1=a1, a2=a2, a3=a3, BHret=BHret, d=d,
+                         meq=meq, eta=eta, zeta=zeta,
                          s2=s2, F=F, observations=observations, age=age,
                          FeH=FeH, m_breaks=m_breaks, vesc=vesc, esc_rate=Mdot_t,
                          tcc=tcc, tracer_masses=tracer_masses,
