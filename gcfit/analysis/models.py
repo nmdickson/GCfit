Hotfix/fix area units (#227)
* fix the unit equivalencies conversion for area

* switch numdens likelihood to use `QuantitySpline`

While the `np.interp` function doesn't destroy the units, I just
feel like the explicitly unit-aware `QuantitySpline` is a bit safer.

* fix bug in Gaussian priors with dependant args

By "bug" I mean "thing I forgot to implement".

* fix bug in scaling in `_plot_data`

Scaling applied in place was affecting the data each time, causing
every plot with scaled data, after the first time, to be incorrect.

* try to fix bug with radius-based units

defining and enabling new units in a decorator can lead to some
awkwardness with the registering of units and some errors when it
tries to duplicate them. This adds a simple check for already
created units to try to avoid this (though it is not perfect).

gcfit/analysis/models.py
@@ -416,12 +416,16 @@ class _ClusterVisualizer:
             eqvs = util.angular_width(self.d)
 
             # Define radius-based units
-            rad_units = [
+            # TODO this starts breaking if you close a plot within the context
+            rad_units = {
                 u.def_unit('r_h', np.median(self.rh)),
                 u.def_unit('r_0', np.median(self.r0)),
                 u.def_unit('r_v', np.median(self.rv)),
                 u.def_unit('r_t', np.median(self.rt))
-            ]
+            }
+
+            rad_units = {ur for ur in rad_units if ur.name not in
+                         [equ.name for equ in u.pc.find_equivalent_units()]}
 
             with (astroviz.quantity_support(),
                   u.set_enabled_equivalencies(eqvs),
@@ -531,7 +535,7 @@ class _ClusterVisualizer:
         # Convert any units desired
         # ------------------------------------------------------------------
 
-        data *= scale
+        data = data * scale
 
         x_domain = self.r if x_data is None else x_data
 
@@ -3421,6 +3425,9 @@ class _ClusterVisualizer:
             # ymedian[np.isnan(ymedian)] = 1.0 << ymedian.unit
 
             ymodel = util.QuantitySpline(xmodel, ymedian)(xdata).to(ydata.unit)
+
+            if model_scale is not None:
+                ymodel *= model_scale[mass_bin]
 
             # --------------------------------------------------------------
             # compute logl

gcfit/util/units.py
@@ -27,22 +27,22 @@ def angular_width(D):
         return np.tan(θ / 2) * (2 * D.value)
 
     # Angular area
-    def pcsq_to_radsq(r):
+    def pcsq_to_radsq(A):
         '''Parsecs squared to radians squared.'''
-        return (1. / rad_to_pc(1)**2) * r
+        return A / D.value**2
 
-    def radsq_to_radsq(θ):
+    def radsq_to_pcsq(Ω):
         '''Radians squared to parsec squared.'''
-        return (1. / pc_to_rad(1)**2) * θ
+        return Ω * D.value**2
 
     # Inverse Angular area
-    def inv_pcsq_to_radsq(r):
+    def inv_pcsq_to_radsq(A_inv):
         '''Parsecs squared reciprocal to radians squared reciprocal.'''
-        return (rad_to_pc(1)**2) * r
+        return A_inv * D.value**2
 
-    def inv_radsq_to_radsq(θ):
+    def inv_radsq_to_radsq(Ω_inv):
         '''Radians squared reciprocal to parsecs squared reciprocal.'''
-        return (pc_to_rad(1)**2) * θ
+        return Ω_inv / D.value**2
 
     # Angular Speed
     def kms_to_asyr(vt):
@@ -55,7 +55,7 @@ def angular_width(D):
 
     return Equivalency([
         (u.pc, u.rad, pc_to_rad, rad_to_pc),
-        (u.pc**2, u.rad**2, pcsq_to_radsq, radsq_to_radsq),
+        (u.pc**2, u.rad**2, pcsq_to_radsq, radsq_to_pcsq),
         (u.pc**-2, u.rad**-2, inv_pcsq_to_radsq, inv_radsq_to_radsq),
         ((u.km / u.s), (u.arcsec / u.yr), kms_to_asyr, asyr_to_kms)
     ], 'angular_width', {"D": D})