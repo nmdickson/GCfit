try to fix bug with radius-based units
defining and enabling new units in a decorator can lead to some
awkwardness with the registering of units and some errors when it
tries to duplicate them. This adds a simple check for already
created units to try to avoid this (though it is not perfect).

gcfit/analysis/models.py
@@ -416,12 +416,16 @@ class _ClusterVisualizer:
             eqvs = util.angular_width(self.d)
 
             # Define radius-based units
-            rad_units = [
+            # TODO this starts breaking if you close a plot within the context
+            rad_units = {
                 u.def_unit('r_h', np.median(self.rh)),
                 u.def_unit('r_0', np.median(self.r0)),
                 u.def_unit('r_v', np.median(self.rv)),
                 u.def_unit('r_t', np.median(self.rt))
-            ]
+            }
+
+            rad_units = {ur for ur in rad_units if ur.name not in
+                         [equ.name for equ in u.pc.find_equivalent_units()]}
 
             with (astroviz.quantity_support(),
                   u.set_enabled_equivalencies(eqvs),